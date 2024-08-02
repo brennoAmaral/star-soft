61684913a7ca52e91ffa6729776ed9f8df49cc94
BrennoDev <38212111+brennoAmaral@users.noreply.github.com>
2024-08-01 21:45:25 -0300

    feat: header an footer

--- src/presenters/pages/page-products/page-products.tsx
+++ src/presenters/pages/page-products/page-products.tsx
@@ -1,19 +1,20 @@
 "use client";
 import cacheProducts from "@/infra/cache/products/cache-products";
 import CardProduct from "./components/card-product";
-
+import style from "./page-products.module.scss";
 export default function PageProducts() {
   const { data, isLoading, error } = cacheProducts.getAll({
     page: 1,
-    limit: 12,
+    limit: 32,
   });
 
   const productsData = data?.data;
 
   return (
-    <section style={{ display: "flex", flexDirection: 'column', justifyItems: 'center', alignItems: 'center'}}>
-      <div style={{display: 'flex', gap: '20px', flexWrap: 'wrap', height: '80vh'}}>
+    <section className={style.section}>
+      <div className={style.containerCards}>
         {productsData?.map((product, index) => {
+          if(index>7) return null
           return <CardProduct key={index} {...product} isLoading={isLoading} />;
         })}
       </div>
