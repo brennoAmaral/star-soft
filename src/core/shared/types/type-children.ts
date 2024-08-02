28c512de18c65529907a9753390cce3320449e62
BrennoDev <38212111+brennoAmaral@users.noreply.github.com>
2024-08-01 23:16:15 -0300

    feat: redux adicionado e iniciando mudança de estrutura de pastas

---
+++ src/core/shared/types/type-children.ts
@@ -0,0 +1,13 @@
+export interface IChildrenNode{
+  children: React.ReactNode
+}
+export interface IChildrensNode{
+  children: React.ReactNode[]
+} 
+
+export interface IChildrenElement{
+  children: React.ReactElement
+}
+export interface IChildrensElement{
+  children: React.ReactElement[]
+} 
